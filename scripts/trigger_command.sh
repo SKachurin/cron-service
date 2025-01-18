ff07420aedbfa4d0d7e69c9ae8de590bd5b9b7cc
Сергей К <9019184@gmail.com>
2025-01-19 00:30:12 +0400

    fix

--- scripts/trigger_command.sh
+++ scripts/trigger_command.sh
@@ -10,10 +10,7 @@ fi
 SECRET_TOKEN="$SECRET_TOKEN"
 
 # Define the URL of the backend endpoint
-BACKEND_URL="http://digital-backend:9000/cron/five-minutes"
-
-# Log the time and action
-echo "$(date): Attempting to POST to $BACKEND_URL"
+BACKEND_URL="http://digital-nginx:80/cron/five-minutes"
 
 # Make the HTTP request to the backend
 if ! curl -X POST "$BACKEND_URL" -H "Authorization: Bearer $SECRET_TOKEN"; then
